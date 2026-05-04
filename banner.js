ea117323b411012293568a3fefc51c285a588065
BSR Studios <aryanmewadastudios@gmail.com>
2026-05-04 10:25:34 -0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,3 @@
+// AnyVidWall - Normal 728x90 Banner Ad Script
+document.write('<script>atOptions = { "key" : "a49c519d5f75e2513650ce959bbf9d65", "format" : "iframe", "height" : 90, "width" : 728, "params" : {} };</scr' + 'ipt>');
+document.write('<script src="https://www.highperformanceformat.com/a49c519d5f75e2513650ce959bbf9d65/invoke.js"></scr' + 'ipt>');
---
+++ nativebanner.js
@@ -0,0 +1,3 @@
+// AnyVidWall - Native Banner Ad Script
+document.write('<script async="async" data-cfasync="false" src="https://pl29336581.profitablecpmratenetwork.com/f25d9f151c824f266821e4ffe1b0b40b/invoke.js"></scr' + 'ipt>');
+document.write('<div id="container-f25d9f151c824f266821e4ffe1b0b40b"></div>');
